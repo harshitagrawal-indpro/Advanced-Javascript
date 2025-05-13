20ceb6ed29c20fcd91196ea8698cd35ee51ff621
harshitagrawal-indpro <harshitagra8092@gmail.com>
2025-05-13 16:03:26 +0530

    datemoduoletry

---
+++ datemoduole.js
@@ -0,0 +1,9 @@
+let now = new Date();
+console.log(now.toString());
+console.log(now.toDateString());
+console.log(now.toTimeString());
+console.log(now.toUTCString());
+console.log(now.toISOString());
+
+let now1 = new Date("2005-10-24");
+console.log(now1);
